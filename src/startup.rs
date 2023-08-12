3d5607441340f3b57bf17d50013f7ef9031e8a40
LE MINH TAM <leminhtam1791998@gmail.com>
2023-08-12 22:56:01 +0700

    Finish chapter 3 of the book

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,21 @@
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
-use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, conneciton_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the pool using web::Data, which boils down to an Arc smart pointer
+    let conneciton_pool = web::Data::new(conneciton_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(conneciton_pool.clone())
     })
     .listen(listener)?
     .run();
+
     Ok(server)
 }
